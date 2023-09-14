1b71132577c7cae953928f2ebd7d6e02af650203
Niekritov Serhii <sergey1991serg@gmail.com>
2023-09-15 02:30:57 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,149 +1,141 @@
-enum EmployeeStatus {
-  Active = 'active',
-  Inactive = 'inactive',
-  UnpaidLeave = 'On unpaid leave',
+//------------------------------------------------------------------------------------//
+// Напишіть функцію isString, яка перевірятиме, чи є передане значення рядком.
+//  Потім використовуйте її для звуження типу змінної.
+//------------------------------------------------------------------------------------//
+const isString = (value: any): value is string => {
+   return  typeof value === "string"
 }
-
-class PreHiredEmployee {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public bankAccountNumber: string
-  ) { }
+//------------------------------------------------------------------------------------//
+// У вас є масив з елементами різних типів. 
+// Напишіть функцію, яка приймає цей масив і фільтрує його так,
+//    щоб у підсумку в ньому залишилися тільки рядки.
+// Використовуйте захисника типу для цього завдання.
+//------------------------------------------------------------------------------------//
+const filterStrings = (arr: any[]): string[] => {
+   return arr.filter(
+      (item): item is string => isString(item)
+      )
 }
-
-class Employee {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public paymentInfo: string,
-    public salary: number,
-    public status: EmployeeStatus,
-    public department: Department
-  ) {}
+//------------------------------------------------------------------------------------//
+// У вас є об'єкт, який може містити довільні властивості.
+// Напишіть функцію, яка приймає цей об'єкт і повертає значення однієї з властивостей,
+//  якщо воно існує і має певний тип.
+//------------------------------------------------------------------------------------//
+const returnValue = (data: any, key: string, expectedType: string): any | undefined  => {
+   if (data[key] && typeof data[key] === expectedType) {
+      return data[key]
+   } else {
+      return undefined
+   }
 }
-
-class Department {
-  protected balance: number = 0;
-  protected employees: Employee[] = [];
-
-  constructor(
-    public name: string,
-    public domain: string,
-    public budget: {
-      debit: number
-      credit: number
-    }
-  ) { }
-
-  calculateBalance(): number {
-    return this.balance + this.budget.debit - this.budget.credit;
-  }
-
-  addPreHiredEmployee(preHiredEmployee: PreHiredEmployee): void {
-    this.assertEmployee(preHiredEmployee);
-    const employee = this.convertToEmployee(preHiredEmployee);
-    this.addEmployee(employee);
-  }
-
-  addEmployee(employee: Employee): void {
-    this.employees.push(employee);
-    this.subtractFromBalance(employee.salary);
-  }
-
-  removeEmployee(employee: Employee): void {
-    this.assertEmployeeActive(employee);
-
-    const index = this.employees.indexOf(employee);
-    if (index !== -1) {
-      this.employees.splice(index, 1);
-      this.balance += employee.salary;
-    }
-  }
-
-  protected getEmployees(): Employee[] {
-    return this.employees;
-  }
-
-  protected subtractFromBalance(amount: number): void {
-    this.balance -= amount;
-  }
-
-  convertToEmployee(preHiredEmployee: PreHiredEmployee): Employee {
-    const employee = new Employee(
-      preHiredEmployee.firstName,
-      preHiredEmployee.lastName,
-      '',
-      0,
-      EmployeeStatus.Active,
-      this
-    );
-    this.addEmployee(employee);
-    return employee;
-  }
-
-  assertEmployeeActive(employee: Employee): asserts employee is Employee {
-    if (employee.status !== EmployeeStatus.Active) {
-      throw new Error('Error: object Employee inactive.');
-    }
-  }
-
-  assertEmployee(employee: Employee | PreHiredEmployee): asserts employee is Employee {
-    if (!(employee as Employee).paymentInfo) {
-      throw new Error('Error: object is not a type Employee.');
-    }
-  }
+//------------------------------------------------------------------------------------//
+// Створіть кілька захисників типу, кожен з яких перевіряє певний аспект об'єкта
+// (наприклад, наявність певної властивості або її тип).Потім напишіть функцію,
+// яка використовує цих захисників у комбінації для звуження типу об'єкта до більш конкретного типу.
+//------------------------------------------------------------------------------------//
+// Захисник типу для перевірки наявності властивості "name"
+const  hasNameProperty = (obj: any): obj is { name: string }=> {
+   return "name" in obj;
+ }
+ 
+ // Захисник типу для перевірки типу властивості "age" як number
+ const hasValidAgeProperty = (obj: any): obj is { age: number } =>{
+   return typeof obj.age === "number";
+ }
+ interface AgeResult {
+   nameResult: string | undefined;
+   ageResult: number | undefined;
+ }
+ // Функція, яка використовує захисники типу для звуження типу об'єкта
+ function processObject(obj: any):AgeResult {
+   let nameResult: string | undefined = undefined;
+   let ageResult: number | undefined = undefined;
+   if (hasNameProperty(obj)) {
+     nameResult = obj.name;
+   }
+   if (hasValidAgeProperty(obj)) {
+     ageResult = obj.age;
+   }
+   return { nameResult, ageResult };
+ }
+//------------------------------------------------------------------------------------//
+// У вас є змінна, яка може бути одного з декількох типів(наприклад, рядок або число).
+// Напишіть функцію, яка приймає цю змінну і виконує довільні операції,
+// специфічні для кожного з типів.
+//------------------------------------------------------------------------------------//
+const performOperation = (input: string | number): string | number | never => {
+   if (typeof input === 'string') {
+     return input
+   } else if (typeof input === 'number') {
+      return input
+   } else {
+      return input
+   }
 }
-
-class AccountingDepartment extends Department {
-  constructor() {
-    super('ACCOUNTING', 'accounting', { debit: 0, credit: 0 });
-  }
-
-  paySalaries(): void {
-    this.getEmployees().forEach((employee): void => {
-      if (employee.status === EmployeeStatus.Active) {
-        this.subtractFromBalance(employee.salary);
-      }
-    });
-  }
-}
-
-class Company {
-  private departments: Department[] = [];
-  private employees: (Employee | PreHiredEmployee)[] = [];
-
-  constructor(public name: string) {}
-
-  addDepartment(department: Department): void {
-    this.departments.push(department);
-  }
-
-  addEmployee(employee: Employee | PreHiredEmployee): void {
-    this.employees.push(employee);
-  }
-}
-
-const company = new Company('My company');
-const accounting = new AccountingDepartment();
-const itDepartment = new Department('IT', 'it', { debit: 10000, credit: 5000 });
-
-company.addDepartment(accounting);
-company.addDepartment(itDepartment);
-
-const preHiredEmployee = new PreHiredEmployee('Donald', 'Moriarty', '1234567890');
-const employee = itDepartment.convertToEmployee(preHiredEmployee);
-company.addEmployee(employee);
-
-console.log('IT department balance sheet:', itDepartment.calculateBalance());
-console.log('Salaries of all staff before payment:', accounting.calculateBalance());
-
-accounting.paySalaries();
-
-console.log('Salary of all staff:', accounting.calculateBalance());
-
-
-
-
-
-
+//------------------------------------------------------------------------------------//
+// Створіть захисник типу, який перевірятиме, чи є передане значення функцією.
+// Потім напишіть функцію, яка використовує цей гард для звуження типу змінної
+// і викликає передану функцію, якщо вона існує.
+//------------------------------------------------------------------------------------//
+const isFunction = (value: any): value is Function =>{
+   return typeof value === 'function';
+ }
+ 
+ const callIfFunction=(func: any): Function | undefined=> {
+   if (isFunction(func)) {
+      return  func(); 
+   } else {
+     
+      return undefined
+   }
+ }
+//------------------------------------------------------------------------------------//
+// Створіть класи з ієрархією успадкування і потім напишіть функцію,
+// яка використовує захисник типу для звуження типу об'єктів,
+// що базуються на цій ієрархії.
+//------------------------------------------------------------------------------------//
+class Animal {
+   name: string;
+ 
+   constructor(name: string) {
+     this.name = name;
+   }
+ }
+ 
+ class Dog extends Animal {
+   bark(): string  {
+      return `bark`
+   }
+ }
+ 
+ class Cat extends Animal {
+   meow(): string  {
+      return `meow`
+   }
+ }
+ 
+ class Bird extends Animal {
+   fly(): string  {
+      return `fly`
+   }
+ }
+ 
+ class Fish extends Animal {
+   swim(): string {
+     return `swim`;
+   }
+ }
+ 
+ const performAction = (animal: Animal): void => {
+   if (animal instanceof Dog) {
+     animal.bark(); 
+   } else if (animal instanceof Cat) {
+     animal.meow(); 
+   } else if (animal instanceof Bird) {
+     animal.fly(); 
+   } else if (animal instanceof Fish) {
+     animal.swim(); 
+   } else {
+      animal.name
+   }
+ }
