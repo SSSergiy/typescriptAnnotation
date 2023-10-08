81d75a66bcfcc2e521c27ddf12f7995ad899a11b
Niekritov Serhii <sergey1991serg@gmail.com>
2023-10-08 18:33:08 +0300

    oen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+var person = {
+    name: 'John',
+    address: {
+        city: 'New York',
+        street: '123 Main St',
+        buildings: {
+            company: {
+                name: 'Amanda',
+                price: 'string',
+                privateCompany: {
+                    name: 'string',
+                },
+                publicCompany: {
+                    name: 'string',
+                    count: 8,
+                },
+            },
+        },
+    },
+};
 var user = {
     name: 'Angelina',
     address: {
