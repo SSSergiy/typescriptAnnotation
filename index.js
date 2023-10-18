d14493fcfbce69ef65d1fb702e85aec0b938cd5d
Niekritov Serhii <sergey1991serg@gmail.com>
2023-10-18 18:08:05 +0300

    oen

--- index.js
+++ index.js
@@ -14,43 +14,36 @@
 // 	 до знаходження паліндрома.Для того, щоб перевірити себе використовуйте
 // 	 число 196. Це так зване Lychrel number — число яке немає поліндрому
 /////////////////////////////////////////////////////////////////////////////////
-var isNumericPalindrome = (number) => {
-  var originalNumber = number;
-  var reversedNumber = 0;
 
-  while (number > 0) {
-    reversedNumber = reversedNumber * 10 + (number % 10);
-    number = Math.floor(number / 10);
+const reverseNumber = (number, reversed = 0) => {
+  if (number === 0) {
+    return reversed;
+  } else {
+    reversed = reversed * 10 + (number % 10);
+    return reverseNumber(Math.floor(number / 10), reversed);
   }
+};
 
+const isNumericPalindrome = (number) => {
+  const originalNumber = number;
+  const reversedNumber = reverseNumber(number);
   return originalNumber === reversedNumber;
 };
 
-var reverseNumber = (num) => {
-  var reversed = 0;
-  while (num > 0) {
-    var lastDigit = num % 10;
-    reversed = reversed * 10 + lastDigit;
-    num = Math.floor(num / 10);
+const numericPalindrome = (number, steps = 0) => {
+  if (isNumericPalindrome(number)) {
+    return {
+      result: number,
+      steps: steps
+    };
+  } else {
+    const reversed = reverseNumber(number);
+    return numericPalindrome(number + reversed, steps + 1);
   }
-  return reversed;
 };
 
-var numericPalindrome = (number) => {
-  var steps = 0;
-
-  while (!isNumericPalindrome(number)) {
-    var reversingNumber = reverseNumber(number);
-    number += reversingNumber;
-    steps++;
-  }
-
-  return {
-    result: number,
-    steps: steps
-  };
-};
 console.log(numericPalindrome(96));
+
 /////////////////////////////////////////////////////////////////////////////////
 // Напишіть функцію, яка приймає масив унікальних елементів і генерує всі
 // можливі перестановки цього масиву.Використовуйте рекурсію для знаходження
@@ -59,41 +52,38 @@ console.log(numericPalindrome(96));
 // [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [2, 3, 1], [3, 1, 2]і[3, 2, 1].
 /////////////////////////////////////////////////////////////////////////////////
 
-var generatePermutations = (arr) => {
-  function permute(current, remaining) {
-    if (remaining.length === 0) {
-      permutations.push(current);
-    } else {
-      var first = remaining[0];
-      var rest = remaining.slice(1);
-
-      var addElement = (newCurrent, newRemaining) => {
-        if (newRemaining.length === 0) {
-          permutations.push(newCurrent);
-        } else {
-          permute(newCurrent, newRemaining);
-        }
-      };
-
-      var i = 0;
-      var addNextElement = () => {
-        if (i < rest.length) {
-          addElement(current.concat(first, rest[i]), rest.slice(0, i).concat(rest.slice(i + 1)));
-          i++;
-          addNextElement();
-        }
-      };
-
-      addElement(current.concat(first), rest);
-      addNextElement();
-    }
+const permutations = (array) => {
+  if (array.length === 0) {
+    return [];
   }
+  if (array.length === 1) {
+    return [array];
+  } else {
+    const result = [];
+    permuteHelper(array, 0, result);
+    return result;
+  }
+};
 
-  var permutations = [];
-  permute([], arr);
-  return permutations;
-}
+const permuteHelper = (array, index, result) => {
+  if (index === array.length) {
+    return result;
+  } else {
+    const first = array[index];
+    const rest = array.slice(0, index).concat(array.slice(index + 1));
+    const subPerms = permutations(rest);
+    result = concatHelper(first, subPerms, 0, result);
+    return permuteHelper(array, index + 1, result);
+  }
+};
 
-console.log(generatePermutations([1, 2, 3, 6]));
+const concatHelper = (first, subPerms, index, result) => {
+  if (index === subPerms.length) {
+    return result;
+  } else {
+    result.push([first].concat(subPerms[index]));
+    return concatHelper(first, subPerms, index + 1, result);
+  }
+};
 
-
+console.log(permutations([1, 2, 3, 6]));
